6fa6531286dd7aaa80ca445fa1332375358a879f
notryanb <notryanb@gmail.com>
2015-04-01 11:21:07 -0400

    Add show single page for a record

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -4,8 +4,13 @@ get '/users/:id' do
   erb :'users/index'
 end
 
-# get '/records/:id' do
-#   @cur_user = User.find_by(id:session[:user_id])
-#   @user_list = User.all
-#   erb :'records/single'
-# end
+get '/records/:id' do
+  @cur_user = User.find_by(id:session[:user_id])
+  @record = Record.find_by(id:params[:id])
+
+  owner_id = @record.user_id
+  @owner = User.find_by(id:owner_id).username
+
+  @comments = @record.comments
+  erb :'records/single'
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :record
+
+  def find_author(user_id)
+    User.find_by(id:user_id).username
+  end
 end
