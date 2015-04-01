b43b140832fdbd076a15d30e6bb4125c0e86ced5
notryanb <notryanb@gmail.com>
2015-04-01 12:04:06 -0400

    Add record form works. Non-Ajax

---
+++ app/controllers/record.rb
@@ -0,0 +1,30 @@
+get '/records/new' do
+  erb :'/records/new'
+end
+
+post '/new' do
+  new_record = Record.new(albumtitle:params[:albumtitle],
+                          artist:params[:artist],
+                          genre:params[:genre],
+                          releaseyear:params[:releaseyear],
+                          owneropinion:params[:owneropinion],
+                          user_id:cur_user.id)
+
+  if new_record.save!
+    redirect "/users/#{cur_user.id}"
+  else
+    [403, 'error']
+  end
+end
+
+
+get '/records/:id' do
+  @record = Record.find_by(id:params[:id])
+
+  owner_id = @record.user_id
+  @owner = User.find_by(id:owner_id).username
+
+  @comments = @record.comments
+  erb :'/records/single'
+end
+
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,16 +1,20 @@
-get '/users/:id' do
-  @cur_user = User.find_by(id:session[:user_id])
+get '/users' do
   @user_list = User.all
   erb :'users/index'
 end
 
-get '/records/:id' do
-  @cur_user = User.find_by(id:session[:user_id])
-  @record = Record.find_by(id:params[:id])
+get '/users/:id' do
 
-  owner_id = @record.user_id
-  @owner = User.find_by(id:owner_id).username
-
-  @comments = @record.comments
-  erb :'records/single'
+  erb :'users/dashboard'
 end
+
+# get '/records/:id' do
+#   @cur_user = User.find_by(id:session[:user_id])
+#   @record = Record.find_by(id:params[:id])
+
+#   owner_id = @record.user_id
+#   @owner = User.find_by(id:owner_id).username
+
+#   @comments = @record.comments
+#   erb :'records/single'
+# end
